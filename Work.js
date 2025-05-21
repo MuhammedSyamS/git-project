6f11c291242d82e328d8a09ded2927348144e30f
Muhammed Syam <shamsaifudheen@gmail.com>
2025-05-21 23:28:56 +0530

    New Editing Becareful

--- Work.js
+++ Work.js
@@ -6,3 +6,15 @@ for(  i=1;i<=10;i++)
     sum=sum+i;
 }
 console.log(i);
+
+
+let grade = "B";
+switch (grade) {
+case "A":
+console.log("Excellent");
+break;
+case "B":
+console.log("Good");
+default:
+console.log("Keep trying");
+}
